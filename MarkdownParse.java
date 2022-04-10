7a7ad0cf2ddb295469a2081fe6e45f64b1da6554
Nidhi Dhamnani <nidhidhamnani@gmail.com>
2022-04-11 02:00:52 +0530

    Create MarkdownParse.java

---
+++ MarkdownParse.java
@@ -0,0 +1,33 @@
+//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class MarkdownParse {
+
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
+        }
+
+        return toReturn;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
+    }
+}
