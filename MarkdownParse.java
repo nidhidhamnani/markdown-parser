374d5ab85234ffd0938fd98a769f5b66e5196706
AlmondHalo <cmrice@ucsd.edu>
2022-04-14 09:47:31 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,12 +11,18 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        if (!markdown.contains("[") || !markdown.contains("]") || !markdown.contains("(") || !markdown.contains(")")) {
+            return toReturn;
+        }
         while(currentIndex < markdown.length()) {
+            int exclamationIndex = markdown.indexOf("!", currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if ((markdown.contains("!") && (openBracket-1 != exclamationIndex)) || !markdown.contains("!")) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
