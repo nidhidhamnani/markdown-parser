7961be7d092464a4ad2edb36329857c8abc9e64b
AlmondHalo <cmrice@ucsd.edu>
2022-04-14 08:52:09 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,7 +19,6 @@ public class MarkdownParse {
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
-
         return toReturn;
     }
 
