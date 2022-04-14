54e14a9bd72f58eb66159342c38a14db985d04d4
Helen <59126064+Miyuki-L@users.noreply.github.com>
2022-04-14 10:50:15 -0700

    Fixed image link identification bug
    Original program treated image source link as url links as well which it shouldn't but not it does not count image source links as urls.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,13 +11,22 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        // System.out.println(currentIndex);
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+            int openBrackPrevIndex = openBracket-1;
+            
+            if (openBrackPrevIndex >=0 && 
+                markdown.substring(openBrackPrevIndex, openBracket).equals("!")) {
+                break;
+            }
+
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
         }
 
         return toReturn;
