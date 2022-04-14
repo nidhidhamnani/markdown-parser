981126b72feef32c4bada35e41d7240354073c0c
Combobyte <varun.sreedhar1@gmail.com>
2022-04-14 13:14:51 -0700

    Added neighbors

--- Graph.java
+++ Graph.java
@@ -1,12 +1,27 @@
 import java.util.ArrayList;
+import java.util.*;
 
 public class Graph {
 
     ArrayList<State> states = new ArrayList<>(); 
+    public final State START;
+    public State currentState;
 
+    public Graph(){
+        START = new State();
+    }
 
     private class State{
-        
+
+        HashMap<String,State> neighbors;
+
+        public State(HashMap neighborList){
+            neighbors = neighborList;
+        }
+
+        public State(){
+
+        }
     }
     
 }
