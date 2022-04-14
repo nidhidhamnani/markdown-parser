bef0fc4375834afad51ec981ee6b504dc696669d
Tyler-Culp <tculp@ucsd.edu>
2022-04-14 13:50:01 -0700

    fixed test file
    MacBook:markdown-parser tylerculp$ javac MarkdownParse.java
    MacBook:markdown-parser tylerculp$ java MarkdownParse test-file.md
    0
    39
    64
    [https://something.com, some-thing.html]
    MacBook:markdown-parser tylerculp$

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,7 +12,7 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         int otherIdx = 0;
-        while(currentIndex < markdown.length()) {
+        while(currentIndex < markdown.length() - 1) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
@@ -20,13 +20,13 @@ public class MarkdownParse {
             if (openParen == -1 || closeParen == -1){
                 break;
             }
-            //if (otherIdx > currentIndex){
-              //  break;
-            //}
+            if (otherIdx > currentIndex){
+                break;
+            }
 
             otherIdx = currentIndex;
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            //System.out.println(currentIndex);
+            System.out.println(currentIndex);
             currentIndex = closeParen + 1;
         }
         System.out.println(currentIndex);
