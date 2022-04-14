d99cf45b6d075facc51bb1c69f60df06e3f6e884
Combobyte <varun.sreedhar1@gmail.com>
2022-04-14 15:00:58 -0700

    Changed to use Graph Class

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -27,7 +27,8 @@ public class MarkdownParse {
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
+        Graph graph = new Graph();
+        ArrayList<String> links = graph.getLinks(content);
 	    System.out.println(links);
     }
 }
