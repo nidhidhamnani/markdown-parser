33727ba5b7447b60f8f7f859dfda08b3b5735ee1
redagent750 <37556745+redagent750@users.noreply.github.com>
2022-04-15 12:52:27 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -30,8 +30,6 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
-        System.out.println("HellO");
-
     }
 }
 
