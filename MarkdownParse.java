0234c6a7d2f82bf46593f557d2d820754b36418a
andrew-onozuka <ryoandrewonozuka@gmail.com>
2022-04-15 09:36:20 -0700

    updates

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,14 +18,13 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
             if (openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1) {
                 break;
             }
-            // System.out.println(currentIndex);
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
         }
-
         return toReturn;
     }
 
--- test-file.md
+++ test-file.md
@@ -1,9 +1,2 @@
-# Title
-
-[link1](https://something.com)
-[link2](some-thing.html)
 
 
-
-[link3](youtube.com)
-[link4](hi)
