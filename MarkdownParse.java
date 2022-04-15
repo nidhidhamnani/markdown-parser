cd5a72523a3abda663f26a8aba8ac0b9f1ddda51
ehsly <ashexiu@gmail.com>
2022-04-14 19:10:44 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,8 +17,8 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
-
         return toReturn;
     }
 
