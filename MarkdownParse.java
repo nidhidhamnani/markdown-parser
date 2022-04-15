b64b9ad59f35f8c42a26d6a1e4a72ef6738080b7
jonathanzhangli <jonathanzhangli20@gmail.com>
2022-04-15 08:54:14 -0700

    Fixed infinite loop bug

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,7 +11,7 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while(currentIndex < markdown.length() - 1) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
