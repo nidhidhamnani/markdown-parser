6d3e2148ae30b6f72f5880db48d16850d951556a
Ayaaa99 <chz011@ucsd.edu>
2022-04-15 17:38:31 -0700

    change to java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,10 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (openBracket== -1 || closeBracket == -1 
+                || openParen == -1 || closeParen == -1){
+                return null;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
