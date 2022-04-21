d5f13002734a568077e57d5bb5b9e8557a859b54
Helen <59126064+Miyuki-L@users.noreply.github.com>
2022-04-21 10:35:18 -0700

    Wrote unit Test on test-file.md
    Path fileName = Path.of("test-file.md");
    
    this link doesn't work when running JUnit through VSCode. to run it through VSCode's testing you need to give the full path. but this works in terminal running.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,25 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class MarkdownParseTest {
    @Test
    public void addition() {
       assertEquals(2, 1 + 1);
+   }
+
+   @Test
+   public void testProvidedTestFile() throws IOException {
+
+      Path fileName = Path.of("test-file.md");
+      String content = Files.readString(fileName);
+      ArrayList<String> links = MarkdownParse.getLinks(content);
+      String[] expected = new String[] {"https://something.com", "some-thing.html"};
+      assertArrayEquals(expected, links.toArray());
+
    }
 }
