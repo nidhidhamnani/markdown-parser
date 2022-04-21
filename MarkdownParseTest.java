d235246a35bebc12931ff59ed4d4769889afd4ad
Helen <59126064+Miyuki-L@users.noreply.github.com>
2022-04-21 10:53:07 -0700

    Refactored the code to utilize readFile helper

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,10 +12,16 @@ public class MarkdownParseTest {
       assertEquals(2, 1 + 1);
    }
 
+   public String readFile(String file) throws IOException {
+      String fullPath = "C:/Users/Helen/Desktop/CSE15L/markdown-parser/" + file;
+      Path fileName = Path.of(fullPath);
+      String content = Files.readString(fileName);
+      return content;  
+   }
+
    @Test
    public void testProvidedTestFile() throws IOException {
-      Path fileName = Path.of("C:/Users/Helen/Desktop/CSE15L/markdown-parser/test-file.md");
-      String content = Files.readString(fileName);
+      String content = readFile("test-file.md");
       ArrayList<String> links = MarkdownParse.getLinks(content);
       String[] expected = new String[] {"https://something.com", "some-thing.html"};
       assertArrayEquals(expected, links.toArray());
@@ -23,8 +29,7 @@ public class MarkdownParseTest {
 
    @Test
    public void testTest2File() throws IOException {
-      Path fileName = Path.of("C:/Users/Helen/Desktop/CSE15L/markdown-parser/test2.md");
-      String content = Files.readString(fileName);
+      String content = readFile("test2.md");
       ArrayList<String> links = MarkdownParse.getLinks(content);
       String[] expected = new String[0];
       assertArrayEquals(expected, links.toArray());
@@ -32,8 +37,7 @@ public class MarkdownParseTest {
 
    @Test
    public void testTestNoLink() throws IOException {
-      Path fileName = Path.of("C:/Users/Helen/Desktop/CSE15L/markdown-parser/testNoLink.md");
-      String content = Files.readString(fileName);
+      String content = readFile("testNoLink.md");
       ArrayList<String> links = MarkdownParse.getLinks(content);
       String[] expected = new String[0];
       assertArrayEquals(expected, links.toArray());
@@ -41,8 +45,7 @@ public class MarkdownParseTest {
 
    @Test
    public void testTestImageAndLink() throws IOException {
-      Path fileName = Path.of("C:/Users/Helen/Desktop/CSE15L/markdown-parser/testImageAndLink.md");
-      String content = Files.readString(fileName);
+      String content = readFile("testImageAndLink.md");
       ArrayList<String> links = MarkdownParse.getLinks(content);
       String[] expected = new String[] {"shouldbeidentified.com"};
       assertArrayEquals(expected, links.toArray());
