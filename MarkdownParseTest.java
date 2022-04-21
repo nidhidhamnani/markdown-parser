427a832f3c85056d0f3981c42b447626864a6099
Helen <59126064+Miyuki-L@users.noreply.github.com>
2022-04-21 10:44:40 -0700

    Wrote tests for the other test files
    Changed the fileNames such that the running Junit test using VSCode testing works and terminal testing also works.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,12 +14,37 @@ public class MarkdownParseTest {
 
    @Test
    public void testProvidedTestFile() throws IOException {
-
-      Path fileName = Path.of("test-file.md");
+      Path fileName = Path.of("C:/Users/Helen/Desktop/CSE15L/markdown-parser/test-file.md");
       String content = Files.readString(fileName);
       ArrayList<String> links = MarkdownParse.getLinks(content);
       String[] expected = new String[] {"https://something.com", "some-thing.html"};
       assertArrayEquals(expected, links.toArray());
+   }
 
+   @Test
+   public void testTest2File() throws IOException {
+      Path fileName = Path.of("C:/Users/Helen/Desktop/CSE15L/markdown-parser/test2.md");
+      String content = Files.readString(fileName);
+      ArrayList<String> links = MarkdownParse.getLinks(content);
+      String[] expected = new String[0];
+      assertArrayEquals(expected, links.toArray());
+   }
+
+   @Test
+   public void testTestNoLink() throws IOException {
+      Path fileName = Path.of("C:/Users/Helen/Desktop/CSE15L/markdown-parser/testNoLink.md");
+      String content = Files.readString(fileName);
+      ArrayList<String> links = MarkdownParse.getLinks(content);
+      String[] expected = new String[0];
+      assertArrayEquals(expected, links.toArray());
+   }
+
+   @Test
+   public void testTestImageAndLink() throws IOException {
+      Path fileName = Path.of("C:/Users/Helen/Desktop/CSE15L/markdown-parser/testImageAndLink.md");
+      String content = Files.readString(fileName);
+      ArrayList<String> links = MarkdownParse.getLinks(content);
+      String[] expected = new String[] {"shouldbeidentified.com"};
+      assertArrayEquals(expected, links.toArray());
    }
 }
