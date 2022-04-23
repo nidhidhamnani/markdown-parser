4f7bf5e1fc01b377a4b00626065c395c6eae50d9
Robintianqili <til024@ucsd.edu>
2022-04-23 15:14:11 -0700

    d

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,5 +1,3 @@
-//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -31,6 +29,6 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
-        System.out.println("12312");
+        
     }
 }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,29 +1,10 @@
 import static org.junit.Assert.*;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-
 import org.junit.*;
 public class MarkdownParseTest {
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
-        
     }
-    @Test
-    public void testgetLink(){
-        try {
-            Path fileName = Path.of("test-file.md");
-        
-        String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
-        String LinkS=links.toString();
-        assertEquals("The website should be printed out correctly","[https://something.com, some-thing.html]", LinkS);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-       
-    }
 }
