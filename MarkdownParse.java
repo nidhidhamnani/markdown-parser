30d23da6173da37a24a86b8345b67be678d2d6d7
Lixin <alixintong@gmail.com>
2022-04-23 16:20:47 -0700

    minup
    initially returned [a-, https://minesweeperonline.com/#200]

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -27,6 +27,9 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (markdown.indexOf(")", closeParen+1) != -1 && markdown.indexOf(")", closeParen+1) < markdown.indexOf("[", closeParen+1)){
+                closeParen = markdown.indexOf(")", closeParen+1);
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1; //0, 0, 30, 24
         }
