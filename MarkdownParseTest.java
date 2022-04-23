7908ba4de3ba86e642cf63b2b9f6c5ac593d4934
Lixin <alixintong@gmail.com>
2022-04-22 19:16:58 -0700

    Created test for MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,25 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class MarkdownParseTest{
+
     @Test
     public void addition(){
         assertEquals(2, 1+1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException{
+        Path fileName = Path.of("/Users/aliceli/Documents/GitHub/markdown-parser/test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<>(); 
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        assertEquals(expected, links);
     }
 }
