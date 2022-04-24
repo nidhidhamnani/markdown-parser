772e48415e014196f0041368e4261551e2a79b99
CM2205 <97645079+Conrado-M-UCSD@users.noreply.github.com>
2022-04-24 14:05:10 -0700

    Commit of forked repo MarkdownParse
    This fork addresses problematic behavior I noticed from the original implementation. New test markdown file "my-test.md" tests these new changes

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,21 +5,65 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+import javax.sound.sampled.LineListener;
+
+/**
+ * Fork of MarkdownParse by Nidhi Dhamnani
+ * Forked by Conrado Martinez
+ * 
+ * This fork addresses some bugs I noticed from 
+ * the original implementation. 
+ * Specifically, this forked version: 
+ * 1) Fixes the bug that would cause an infinite 
+ *    loop if the last line of the markdown file 
+ *    was blank. 
+ * 2) Filters out images (which use the ![Image] tag option)
+ * 3) Filters out text that is not a link (text in parenthesis w/o 
+ *    [http:, https:, www.] will be ignored)
+ * 
+ * these behaviors are tested in the newly added test file 
+ * my-test.md
+ */
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        int openBracket;
         while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
+            System.out.println(currentIndex);
+
+            // Handles edge case where an image tag contains the last open bracket
+            if (currentIndex == -1) {
+                System.out.println("searched item not found. exiting");
+                break;
+            }
+            // Fix for infinite loop caused by empty last line
+            if (currentIndex + 1 == markdown.length() && markdown.charAt(currentIndex) == '\n'){
+                break;
+            }
+
+            openBracket = markdown.indexOf("[", currentIndex);
+            // Check for ! to determine if the item is a non-link tag
+            if (openBracket != 0 && markdown.charAt(openBracket - 1) == '!') {
+                currentIndex = markdown.indexOf('[', openBracket + 1);
+                continue;
+            }
+
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+
+            // Filtering out non-link uses of tags
+            String linkToAdd = markdown.substring(openParen + 1, closeParen);
+            if (linkToAdd.substring(0, 6).equalsIgnoreCase("HTTPS:") ||
+                linkToAdd.substring(0, 5).equalsIgnoreCase("HTTP:") ||
+                linkToAdd.substring(0, 4).equalsIgnoreCase("www.")) {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen).toLowerCase());
+            }
             currentIndex = closeParen + 1;
         }
-
         return toReturn;
     }
 
---
+++ my-test.md
@@ -0,0 +1,9 @@
+#Title
+
+Here is my test file! 
+[Link to site](hTtps://hello.com)
+[Link [no] 2](httPs://wikipedia.com)
+[link3](https://ucsd.edu)
+![Image](Coolphoto.jpg)
+[link4](https://hi.com)
+![Image](Portrait.png)
