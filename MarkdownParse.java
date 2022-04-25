5ffbebadfa6835ca8e814f4a49fa05ddaeed0df4
redagent750 <37556745+redagent750@users.noreply.github.com>
2022-04-25 12:42:05 -0800

    While loop
    fixed the infinite while loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,9 +18,10 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-            if(markdown.substring(currentIndex).isBlank() && currentIndex != 0){
+            int ExMark = markdown.indexOf("!",openBracket-1);
+            currentIndex = closeParen + 1;        
+
+            if(openBracket == -1 || openParen == -1 || closeParen == -1){
                 break;
             }
         }
