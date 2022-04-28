e528a52a9f99bf6c2b3e987595e42255b6968d79
Tyler-Culp <tculp@ucsd.edu>
2022-04-28 12:55:10 -0700

    JUnit codes at bottom

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -107,3 +107,5 @@ public class MarkdownParseTest {
     }
 }
 
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java MarkdownParse.java
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
