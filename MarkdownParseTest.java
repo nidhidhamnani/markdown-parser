e489724c841a489095dcf7c1ea8f8cf9cb4c8910
Combobyte <varun.sreedhar1@gmail.com>
2022-04-28 13:35:42 -0700

    Changed tests to use static version

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -16,32 +16,28 @@ public class MarkdownParseTest {
     @Test
     public void MarkdownParseTest() throws IOException{
         ArrayList<String> expected = new ArrayList<>(List.of("https://something.com", "some-thing.html"));
-        MarkdownParse mp = new MarkdownParse();
-        ArrayList<String> resultArray = mp.getLinks(Files.readString(Path.of("test-file.md")));
+        ArrayList<String> resultArray = MarkdownParse.getLinks(Files.readString(Path.of("test-file.md")));
         assertEquals(expected, resultArray);
     }
 
     @Test
     public void MarkdownParseTest2() throws IOException{
         ArrayList<String> expected = new ArrayList<>();
-        MarkdownParse mp = new MarkdownParse();
-        ArrayList<String> resultArray = mp.getLinks(Files.readString(Path.of("break-file.md")));
+        ArrayList<String> resultArray = MarkdownParse.getLinks(Files.readString(Path.of("break-file.md")));
         assertEquals(expected, resultArray);
     }
 
     @Test
     public void MarkdownParseTest3() throws IOException{
         ArrayList<String> expected = new ArrayList<>();
-        MarkdownParse mp = new MarkdownParse();
-        ArrayList<String> resultArray = mp.getLinks(Files.readString(Path.of("break-file2.md")));
+        ArrayList<String> resultArray = MarkdownParse.getLinks(Files.readString(Path.of("break-file2.md")));
         assertEquals(expected, resultArray);
     }
 
     @Test
     public void MarkdownParseTest4() throws IOException{
         ArrayList<String> expected = new ArrayList<>(List.of("someLink.com"));
-        MarkdownParse mp = new MarkdownParse();
-        ArrayList<String> resultArray = mp.getLinks(Files.readString(Path.of("break-file3.md")));
+        ArrayList<String> resultArray = MarkdownParse.getLinks(Files.readString(Path.of("break-file3.md")));
         assertEquals(expected, resultArray);
     }
 
@@ -56,16 +52,14 @@ public class MarkdownParseTest {
     @Test
     public void MarkdownParseTest6() throws IOException{
         ArrayList<String> expected = new ArrayList<>(List.of());
-        MarkdownParse mp = new MarkdownParse();
-        ArrayList<String> resultArray = mp.getLinks(Files.readString(Path.of("test-file3.md")));
+        ArrayList<String> resultArray = MarkdownParse.getLinks(Files.readString(Path.of("test-file3.md")));
         assertEquals(expected, resultArray);
     }
 
     @Test
     public void MarkdownParseTest7() throws IOException{
         ArrayList<String> expected = new ArrayList<>(List.of());
-        MarkdownParse mp = new MarkdownParse();
-        ArrayList<String> resultArray = mp.getLinks(Files.readString(Path.of("test-file3.md")));
+        ArrayList<String> resultArray = MarkdownParse.getLinks(Files.readString(Path.of("test-file3.md")));
         assertEquals(expected, resultArray);
     }
 
