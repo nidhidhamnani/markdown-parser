53382e3b07165c766617858580251fb447d21a01
Omartinezangulo <103291599+Omartinezangulo@users.noreply.github.com>
2022-05-08 23:31:22 -0700

    update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -51,7 +51,7 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
-        System.out.println("hi");
+
         
     }
 }
