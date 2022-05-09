edcfd9315d08a15d4374482cbec02f0a348fd825
Nidhi Dhamnani <nidhidhamnani@gmail.com>
2022-05-08 22:56:56 -0700

    3pm MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,23 +12,22 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            // System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
+            System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(nextOpenBracket == -1 || nextCloseBracket == -1 ||
-                     openParen == -1 || closeParen == -1) {
-                break;
+            if(nextOpenBracket == -1 || nextCloseBracket == -1
+                  || closeParen == -1 || openParen == -1) {
+                return toReturn;
             }
-            if(nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') {
+            String potentialLink = markdown.substring(openParen + 1, closeParen);
+            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
+                toReturn.add(potentialLink);
+                currentIndex = closeParen + 1;
+            }
+            else {
                 currentIndex = currentIndex + 1;
-                continue;
             }
-            String link = markdown.substring(openParen + 1, closeParen);
-            if(link.indexOf(" ") == -1) {
-                toReturn.add(link);
-            }
-            currentIndex = closeParen + 1;
         }
         return toReturn;
     }
