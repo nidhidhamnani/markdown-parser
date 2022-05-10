1b663415d4326106daf089319b1872cc8aa717fe
AngeliaZddl <78582332+AngeliaZddl@users.noreply.github.com>
2022-05-10 04:30:38 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -35,7 +35,6 @@ public class MarkdownParse {
 
 
     public static void main(String[] args) throws IOException {
-	if (args.length==0) return;
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
