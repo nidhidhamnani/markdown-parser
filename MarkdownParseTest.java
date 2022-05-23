f92eeaba1c97f2e69e5b9c384c8a2223c68a136d
JISOLPARK <92129615+JISOLPARK@users.noreply.github.com>
2022-05-22 23:24:00 -0700

    snippet tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -46,4 +46,36 @@ public class MarkdownParseTest {
         String content = Files.readString(fileName);
         assertEquals(toReturn, MarkdownParse.getLinks(content));
     }
+    @Test 
+    public void testSnippet1() throws IOException{
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("'google.com");
+        MarkdownParse snippet1 = new MarkdownParse();
+        Path fileName = Path.of("snippet1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actual = snippet1.getLinks(fileName);
+        assertEquals(expected,actual);
+    }
+    @Test 
+    public void testSnippet2() throws IOException{
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a.com");
+        expected.add("a,com(())");
+        expected.add("example.com");
+        MarkdownParse snippet2 = new MarkdownParse();
+        Path fileName = Path.of("snippet2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actual = snippet2.getLinks(fileName);
+        assertEquals(expected,actual);
+    }
+    @Test 
+    public void testSnippet3() throws IOException{
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        MarkdownParse snippet3 = new MarkdownParse();
+        Path fileName = Path.of("snippet3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actual = snippet3.getLinks(fileName);
+        assertEquals(expected,actual);
+    }
 }
