177fdc7789ed36ac8df16a390ccbe75ccbfd56ec
Helen <59126064+Miyuki-L@users.noreply.github.com>
2022-06-02 10:49:31 -0700

    Added CommonMark's WordCountVisitor example

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -8,6 +8,31 @@ class TryCommonMark {
         Node document = parser.parse("This is *Sparta*");
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         String content = renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
-        System.out.println(content);
+      //   System.out.println(content);
+
+         //  # this part actually does the computation
+         Node node = parser.parse("Example\n=======\n\nSome more text");
+         WordCountVisitor visitor = new WordCountVisitor();
+         node.accept(visitor);
+         // visitor.wordCount;  // 4
+         System.out.println(visitor.wordCount);
+    }
+
+   
+}
+
+// # this class can be defined anywhere in the file
+class WordCountVisitor extends AbstractVisitor {
+   int wordCount = 0;
+
+    @Override
+    public void visit(Text text) {
+        // This is called for all Text nodes. Override other visit methods for other node types.
+
+        // Count words (this is just an example, don't actually do it this way for various reasons).
+        wordCount += text.getLiteral().split("\\W+").length;
+
+        // Descend into children (could be omitted in this case because Text nodes don't have children).
+        visitChildren(text);
     }
 }
